98e3173f6a2c8a0b5f01450b4cfcc265e413c3c1
Josh Brown <joshbrown8159@gmail.com>
2024-10-05 17:11:07 +0100

    Added gas damage and bug death

--- bug/bug.gd
+++ bug/bug.gd
@@ -1,10 +1,12 @@
 class_name Bug extends CharacterBody2D
 
 var target : Player
+var in_gas = false
+var is_dead = false
+@export var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	pass # Replace with function body.
 
 
@@ -13,3 +15,22 @@ func _process(delta):
 	target = get_tree().get_first_node_in_group("player")
 	global_position = global_position.lerp(target.global_position, 0.01)
 	move_and_slide()
+
+	if (in_gas and in_gas.emitting):
+		health = health - 1
+	
+	if (health == 0 and !is_dead):
+		is_dead = true
+		queue_free()
+
+	print(health)
+
+
+func _on_area_2d_area_entered(area:Area2D):
+	if (area.get_parent().is_in_group('gas')):
+		in_gas = area.get_parent();
+
+
+func _on_area_2d_area_exited(area):
+	if (area.get_parent().is_in_group('gas')):
+		in_gas = false;
--- player/player.gd
+++ player/player.gd
@@ -2,12 +2,23 @@ class_name Player extends CharacterBody2D
 
 @export var health = 100
 @export var speed = 400
+var gas: CPUParticles2D
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+func _ready():
+	gas = get_node('gas')
+
+func _process(delta):
+	if (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		self.gas.emitting = true
+	else:
+		self.gas.emitting = false
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
-	rotation = get_global_mouse_position().angle_to_point(position)
+	# rotation = get_global_mouse_position().angle_to_point(position) - 180
+	look_at(get_global_mouse_position())
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
